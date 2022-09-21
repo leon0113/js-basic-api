ecf368d021a8831ac49f4c4061455752f3cfc8fb
leon0113 <tahjib.leon399@gmail.com>
2022-09-22 05:52:37 +0600

    more fetch

--- api.js
+++ api.js
@@ -3,3 +3,21 @@ function loadData() {
         .then(respose => respose.json())
         .then(json => console.log(json))
 }
+// call from another function
+function loadUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(reponse => reponse.json())
+        .then(data => displayUsers(data))
+}
+
+function displayUsers(data) {
+    console.log(data);
+}
+
+
+function loadPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(reponse => reponse.json())
+        .then(postData => console.log(postData))
+}
+
