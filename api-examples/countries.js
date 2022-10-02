bdc0b9faab9b8e628b17570c773440648e2f0d47
leon0113 <tahjib.leon399@gmail.com>
2022-10-03 05:48:27 +0600

    country api dynamic

--- api-examples/countries.js
+++ api-examples/countries.js
@@ -11,15 +11,39 @@ const displayCountries = countries => {
     //     console.log(country);
     // }
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name.common;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = `The capital of ${country.name.common} is : ${country.capital} and the country share borders with ${country.borders} `
-        div.appendChild(p);
+        div.innerHTML = `
+      <h3>${country.name.common}</h3>
+      <p>The Capital city is:${country.capital}</p>
+      <button onclick="loadCountryDetails('${country.name.common}')">Details</button>
+      `;
+
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name.common;
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = `The capital of ${country.name.common} is : ${country.capital} and the country share borders with ${country.borders} `
+        // div.appendChild(p);
         countriesDiv.appendChild(div);
     })
+}
+
+const loadCountryDetails = name1 => {
+    const url = `https://restcountries.com/v3.1/name/${name1}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetails(data[0]));
+}
+
+
+const displayCountryDetails = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('country-details');
+    countryDiv.innerHTML = ` 
+    <h3> Country details</h3>
+        The population of ${country.name.common} is : ${country.population} <br>
+    <img src="${country.flags.png}">
+        `;
 }
